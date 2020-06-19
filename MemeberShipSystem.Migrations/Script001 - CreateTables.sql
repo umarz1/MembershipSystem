d47c5ca35b7da2156416cd716d6288482be5d7ac
Muhammad Umar Zahir <muhammad.zahir@sainsburys.co.uk>
2020-06-19 19:23:44 +0100

    Initial migrationj

---
+++ MemeberShipSystem.Migrations/Script001 - CreateTables.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE Users (
+	UserId VARCHAR(200) NOT NULL CONSTRAINT PK_Users PRIMARY KEY,
+	EmployeeId VARCHAR(200) NOT NULL,
+	Name NVARCHAR(200) NOT NULL,
+	Email NVARCHAR(200) NOT NULL,
+	Mobile NVARCHAR(200) NOT NULL,
+)
+GO
+
+CREATE TABLE Employees (
+	EmployeeId VARCHAR(200) NOT NULL CONSTRAINT PK_Employees PRIMARY KEY,
+	Name NVARCHAR(200) NOT NULL,
+    Role NVARCHAR(200) NOT NULL,
+	Email NVARCHAR(200) NOT NULL,
+	Mobile NVARCHAR(200) NOT NULL,
+)
+GO
+
+INSERT INTO Users
+           (UserId
+           ,EmployeeId
+           ,Name
+           ,Email
+           ,Mobile)
+     VALUES
+           ('1234',
+           '123222',
+           'John',
+            'Test email',
+           '01112458')
+GO
